2b3a004a7caffe2484949d852b6f18d430e269bd
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-26 22:52:28 +0800

    use FileSystemWatcher to monitor the source folder

--- FileCopyService/Worker.cs
+++ FileCopyService/Worker.cs
@@ -1,24 +1,44 @@
+using FileCopyService.Interface;
+
 namespace FileCopyService
 {
-    public class Worker : BackgroundService
+    public class Worker(ILogger<Worker> logger, ICopyService copyService) : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<Worker> _logger = logger;
+        private readonly ICopyService _copyService = copyService;
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            string sFolder = AppSettingHelper.GetValue("Folders:Source");
+            string dFolder = AppSettingHelper.GetValue("Folders:Destination");
+
+            _copyService.CreateDir(sFolder, dFolder);
+
+
+            FileSystemWatcher watcher1 = new()
+            {
+                Path = sFolder,
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName
+            };
+            watcher1.Created += (sender, e) => _copyService.OnChanged(e, dFolder);
+            watcher1.EnableRaisingEvents = true;
+
+            #region UPDATE LOG FILE
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    _logger.LogInformation("Copying service running at: {time}", DateTimeOffset.Now);
+
+                    if (_copyService.IsFolderEmpty(sFolder))
+                    {
+                        _logger.LogInformation("Source folder is empty.");
+                    }
                 }
-                await Task.Delay(2000, stoppingToken);
+                //update the log file every 10s
+                await Task.Delay(10000, stoppingToken);
             }
+            #endregion
         }
     }
 }
