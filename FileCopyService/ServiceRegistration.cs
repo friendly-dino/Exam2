81f1861b8a3101bc0bd1233510f0aee7331773c5
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-26 12:54:40 +0800

    implement Serilog for easier logging

--- FileCopyService/ServiceRegistration.cs
+++ FileCopyService/ServiceRegistration.cs
@@ -1,13 +1,19 @@
 ﻿using FileCopyService.Interface;
 using FileCopyService.Service;
 using Microsoft.Extensions.Logging.EventLog;
-
+using Serilog;
 namespace FileCopyService
 {
     public static class ServiceRegistration
     {
         public static HostApplicationBuilder AddRequiredServices(this HostApplicationBuilder builder)
         {
+            Log.Logger = new LoggerConfiguration()
+           .WriteTo.File(AppSettingHelper.GetValue("Folders:Log"), rollingInterval: RollingInterval.Hour)
+           .WriteTo.EventLog("FileCopyService", manageEventSource: true)
+           .CreateLogger();
+
+            builder.Logging.ClearProviders().AddSerilog();
             builder.Logging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Information);
             builder.Services.AddHostedService<Worker>();
             builder.Services.AddSingleton<ICopyService, CopyService>();
