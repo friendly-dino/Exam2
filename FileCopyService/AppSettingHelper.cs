7d9d0a25d4c31ed480f1ab00e0afb792932e7b3f
Jansen Magbojos <Jansen.Magbojos@PH.GT.COM>
2024-06-26 11:39:58 +0800

    define setting and getting the folder path

---
+++ FileCopyService/AppSettingHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace FileCopyService
+{
+
+    public static class AppSettingHelper
+    {
+        private static IConfigurationRoot configuration;
+        static AppSettingHelper()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            configuration = builder.Build();
+        }
+
+        public static string GetValue(string key)
+        {
+            return configuration[key];
+        }
+    }
+}
